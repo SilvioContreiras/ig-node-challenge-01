19422e602fbb0e6436f39085115a9c5218e2123c
Silvio Cajinca Contreiras <SCContreiras@rd.com.br>
2021-11-20 19:01:08 -0300

    initial config

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).send({ message: "User not found" });
+  }
+
+  return next();
+}
+
+app.post("/users", (request, response) => {
+  const { name, userName } = request.body;
+
+  const customerAlreadyExists = users.some(
+    (user) => user.username === userName
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(404).send({ message: "User already exists" });
+  }
+
+  users.push({
+    id: uuidv4(),
+    name,
+    userName,
+    todos: [],
+  });
+});
+
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.post("/todos", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+module.exports = app;
